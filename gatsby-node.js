1acbba052f5c065070881190b7c7769190febc17
Jimmy Boinembalome <jboinembalome@gmail.com>
2022-07-29 15:46:44 +0200

    Add page creation for articles and tags

---
+++ gatsby-node.js
@@ -0,0 +1,121 @@
+/**
+ * Gatsby gives plugins and site builders many APIs for building your site. 
+ * Code in the file gatsby-node is run once in the process 
+ * of building your site. You can use its APIs to 
+ * create pages dynamically, add data into GraphQL
+ * or respond to events during the build lifecycle. 
+ * (More info: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/)
+ */
+
+const _ = require('lodash');
+const path = require(`path`);
+const { createFilePath } = require(`gatsby-source-filesystem`);
+
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions;
+
+  const result = await graphql(`
+    {
+      allMarkdownRemark(limit: 1000) {
+        edges {
+          node {
+            id
+            fields {
+              slug
+            }
+            frontmatter {
+              tags
+              templateKey
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  if (result.errors) {
+    reporter.panicOnBuild(
+      `There was an error loading your blog posts`,
+      result.errors
+    );
+
+    result.errors.forEach((e) => console.error(e.toString()));
+    return Promise.reject(result.errors);
+  }
+
+  const posts = result.data.allMarkdownRemark.edges;
+
+  // Create pages based on templateKey
+  posts.forEach((edge) => {
+    const id = edge.node.id;
+    createPage({
+      path: edge.node.fields.slug,
+      tags: edge.node.frontmatter.tags,
+      component: path.resolve(
+        `src/templates/${String(edge.node.frontmatter.templateKey)}.js`
+      ),
+      // additional data can be passed via context
+      context: {
+        id,
+      },
+    });
+  });
+
+  // Create paginated blog list pages
+  const postsPerPage = 2;
+  const numPages = Math.ceil(posts.length / postsPerPage);
+  Array.from({ length: numPages }).forEach((_, i) => {
+    const firstPage = i === 0;
+    const currentPage = i + 1;
+    createPage({
+      path: firstPage ? "/blog" : `/blog/page/${currentPage}`,
+      component: path.resolve("./src/templates/blog.js"),
+      context: {
+        limit: postsPerPage,
+        skip: i * postsPerPage,
+        numPages,
+        currentPage: i + 1,
+      },
+    });
+  });
+
+  // Tag pages:
+  let tags = [];
+  // Iterate through each post, putting all found tags into `tags`
+  posts.forEach((edge) => {
+    if (_.get(edge, `node.frontmatter.tags`)) {
+      tags = tags.concat(edge.node.frontmatter.tags);
+    }
+  });
+  // Eliminate duplicate tags
+  tags = _.uniq(tags);
+
+  // Make tag pages
+  tags.forEach((tag) => {
+    const tagPath = `/tags/${_.kebabCase(tag)}/`;
+
+    createPage({
+      path: tagPath,
+      component: path.resolve(`src/templates/tags.js`),
+      context: {
+        tag,
+      },
+    });
+  });
+};
+
+
+// Create a slug field that contains the URL of the page
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions;
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode });
+
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    });
+  }
+};
