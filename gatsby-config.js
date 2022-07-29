ac20843c04148c8768b2307a4393fc4d32ebccc6
Jimmy Boinembalome <jboinembalome@gmail.com>
2022-07-29 15:45:05 +0200

    Add site metadata and plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,46 @@
+/**
+ * The file gatsby-config defines your site’s metadata, plugins
+ * and other general configuration. 
+ * (More info: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/)
+ */
+
 module.exports = {
   siteMetadata: {
     title: `The Frog Blog`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-postcss", "gatsby-plugin-react-helmet", "gatsby-transformer-remark", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+    author: {
+      name: `Guyliane`,
+      summary: `Hi! 👋 I'm Guyliane, I've been passionate about development for so long that I've spent many sleepless nights exploring our favorite site like Stack Overflow to get unstuck.`,
     },
-    __key: "pages"
-  }]
+    description: `Welcome to The Frog Blog, I decided to open my Blog to share with you all my tips all my problems all my findings so that you too can sleep a little more! 🤞`,
+    siteUrl: `https://www.thefrogblog.uk`,
+    social: {
+      twitter: `guyliane`,
+    },
+  },
+  plugins: [
+    "gatsby-plugin-netlify-cms",
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-react-helmet",
+    "gatsby-transformer-remark",
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/static`,
+        name: 'assets',
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
+      },
+    },
+    {
+      resolve: `gatsby-plugin-disqus`,
+      options: {
+        shortname: `https-thefrogblog-netlify-app`
+      }
+    }
+  ],
 };
