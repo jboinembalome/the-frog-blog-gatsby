90ddf1fccfd320912d9630e2e7d6e1ba12dcc6aa
Jimmy Boinembalome <jboinembalome@gmail.com>
2022-07-29 16:27:43 +0200

    Add SearchBlogs in Tag page

--- src/templates/tag.js
+++ src/templates/tag.js
@@ -2,6 +2,7 @@ import React from "react";
 import { graphql } from "gatsby";
 import Blog from "../components/blog/Blog";
 import Seo from "../components/shared/Seo";
+import SearchBlogs from "../components/blog/SearchBlogs";
 
 const TagTemplatePage = ({
   data: { allMarkdownRemark: { edges: posts } },
@@ -9,6 +10,9 @@ const TagTemplatePage = ({
 }) => (
   <>
     <h1 className="text-center text-5xl font-extrabold mt-8">{tag}</h1>
+    
+    <SearchBlogs />
+
     {posts &&
       posts.map(({ node: post }) => (
         <Blog key={post.id} {...post.frontmatter} slug={post.fields.slug} />
