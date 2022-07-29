d1aaa2731ed4ed3c94198fce4654cde53a001e57
Jimmy Boinembalome <jboinembalome@gmail.com>
2022-07-29 16:10:03 +0200

    Add template for Tag page

---
+++ src/templates/tags.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { graphql } from "gatsby";
+import Blog from "../components/blog/Blog";
+import Seo from "../components/shared/Seo";
+
+const TagRoute = ({
+  data: { allMarkdownRemark: { edges: posts } },
+  pageContext: { tag }
+}) => (
+  <>
+    <h1 className="text-center text-5xl font-extrabold mt-8">{tag}</h1>
+    {posts &&
+      posts.map(({ node: post }) => (
+        <Blog key={post.id} {...post.frontmatter} slug={post.fields.slug} />
+      ))}
+  </>
+);
+
+export const Head = ({ pageContext }) => <Seo title={pageContext.tag} />;
+
+export default TagRoute;
+
+export const tagPageQuery = graphql`
+  query TagPage($tag: String) {
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { tags: { in: [$tag] } } }
+    ) {
+      edges {
+        node {
+          id
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            templateKey
+            date(formatString: "ddd, MMM D, YYYY")
+            tags
+          }
+        }
+      }
+    }
+  }
+`;
